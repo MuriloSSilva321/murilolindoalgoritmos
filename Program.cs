710ea5066f365dc834cf5079843b818113b4e522
MuriloSSilva321 <eduardoedasilva26@gmail.com>
2026-05-11 11:41:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Algoritmo que classifica um número qualquer em uma 
+
+
+
+int numero = 0;
+
+
+
+Console.WriteLine("Digite um número qualquer");
+numero = Convert.ToInt32(Console.ReadLine());
+
+switch(numero)
+{
+    case >=0 and <=25:
+    Console.WriteLine("Está no intervalo [0..25]");
+    break;
+    
+    case >25 and <=50:
+    Console.WriteLine("O número está no intervalo de 25..50");
+    break;
+
+    case >50 and <=75:
+    Console.WriteLine("O número está no intervalo 50..75");
+    break;
+    
+    case >75 and <=100:
+    Console.WriteLine("O número está no intervalo de 75..100");
+    break;
+
+    default:
+    Console.WriteLine("O número está fora do intervalo!!!");
+    break;
+}
